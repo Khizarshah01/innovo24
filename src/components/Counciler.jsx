85396714fa0987e9070bd3cd469b2c92ca3fc277
khizarshah01 <khizar.shah010@gmail.com>
2024-02-18 01:41:43 +0530

    add a shity counciler book page at 16:34

--- src/components/Counciler.jsx
+++ src/components/Counciler.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
+import React from 'react';
+import "../style/Counciler.css";
+import emp from './images/employee.svg'
 function Counciler() {
-  return (
-    <div>Counciler</div>
-  )
+    return (
+        <>
+        <div className='card-tank'>
+
+            <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+
+            <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+
+
+        <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+
+        </div>
+        <div className="card-tank">
+
+        <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+
+
+        <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+
+
+            <div className="card">
+                <img src={emp} alt="Avatar" id='card-image' />
+                <div className="card-container">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default Counciler
