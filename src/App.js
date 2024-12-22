276bfd34d7d801599e7183ce45c51a1ec29ef34a
Khizarshah01 <khizar.shah010@gmail.com>
2024-12-22 23:00:47 +0530

    adding for temp purpose

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import CouncilPro from "./components/CounselorProfile";
 import { io } from "socket.io-client";
 
 
-const socket = io('<http://localhost:3000>');
+// const socket = io('<http://localhost:3000>');
 
 function App() {
     return (
